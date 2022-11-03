12166711ccbb98173c3a2d3bab05442ba6fd10b6
Breno Brito <breno.silvabrito@hotmail.com>
2022-11-03 08:34:05 +0000

    Implement users login/logout

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', include('training.urls')),
-    path('training/', include('training.urls')),
+    path('', include('training.urls'), name='home'),
+    path('training/', include('training.urls'), name='home'),
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('accounts/login', TemplateView.as_view(template_name='login.html'), name='login')
 ]
